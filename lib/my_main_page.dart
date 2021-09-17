7213f8de641cb15d9569ffe9144d328e63dbfcc0
Nathan <nathanthientran@gmail.com>
2021-09-17 06:28:16 -0700

    Messing around with the UI
    Still trying to figure out the best look for the app. Its fun but hard at the same time.

--- lib/my_main_page.dart
+++ lib/my_main_page.dart
@@ -11,6 +11,8 @@ class MyMainPage extends StatefulWidget {
 
 class _MyMainPageState extends State<MyMainPage> {
 
+  final List<String> entries = <String>['Recipe 1', 'Recipe2', 'Recipe 3'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,18 +32,46 @@ class _MyMainPageState extends State<MyMainPage> {
         //Gotta add some things later here
         //Still have to decide what settings or features are gonna be here
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('Recipe 1'),
-          ),
-          ListTile(
-            title: Text('Recipe 2'),
-          ),
-          ListTile(
-            title: Text('Recipe 3'),
-          ),
-        ]
+      body: ListView.builder(
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index){
+          return ListTile(
+              title: Container(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  margin: EdgeInsets.all(10.0),
+                  color: Colors.tealAccent,
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap:(){
+                      print('Tapped');
+                    },
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Image(
+                            color: Colors.teal,
+                            colorBlendMode: BlendMode.color,
+                            height: 100,
+                            width: 100,
+                            image: NetworkImage('https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081'),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(20.0),
+                          child: Text(
+                            '${entries[index]}'
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+          );
+        }
       ),
     );
   }
